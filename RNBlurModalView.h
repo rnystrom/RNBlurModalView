3895b2b5a5be89e63188e0f67638c9cca6625e5d
Jose Ramon Cano <jose.cano@theconsultancyfirm.com>
2013-10-09 15:45:11 +0200

    Adding AppleDoc
    Just adding dummy comments to the .h to supress the warnings from the
    AppleDoc compiler.

--- RNBlurModalView.h
+++ RNBlurModalView.h
@@ -26,7 +26,9 @@
 #import <UIKit/UIKit.h>
 #import <Foundation/Foundation.h>
 
+/** The string for the NSNotificationCenter. Will be called when the window is shown */
 extern NSString * const kRNBlurDidShowNotification;
+/** The string for the NSNotificationCenter. Will be called when the window is hidden */
 extern NSString * const kRNBlurDidHidewNotification;
 
 /**
@@ -42,14 +44,22 @@ typedef NS_ENUM(NSUInteger, RNBlurModalViewAlignment){
     RNBlurModalViewAlignmentBottom      = 2,
 };
 
+/**
+ * A Modal view to hold content, blurring all the background
+ */
 @interface RNBlurModalView : UIView
 
+/** Tells the user if the window is showing. */
 @property (assign, readonly) BOOL isVisible;
-
+/** Specifies the duration of the animation. */
 @property (assign) CGFloat animationDuration;
+/** Specifies the delay before the animation starts. */
 @property (assign) CGFloat animationDelay;
+/** Animation options */
 @property (assign) UIViewAnimationOptions animationOptions;
+/** Changes the position of the button from the left (FALSE) to the right (TRUE) */
 @property (assign) BOOL dismissButtonRight;
+/** Block called when the view is hidden */
 @property (nonatomic, copy) void (^defaultHideBlock)(void);
 /** Alignment of the content view (left, center or right) */
 @property (assign, nonatomic) RNBlurModalViewAlignment horizontalAlignment;
@@ -62,20 +72,87 @@ typedef NS_ENUM(NSUInteger, RNBlurModalViewAlignment){
  * if VerticalAlignment == NSBlurModalViewAlignmentCenter it will be ignored */
 @property (assign, nonatomic) CGFloat verticalSpacing;
 
+/**
+ * Constructor with parent view controller and custom content
+ * @param viewController The parent view controller
+ * @param view The content to show
+ * @return self or nil if something went wrong
+ */
 - (id)initWithViewController:(UIViewController*)viewController view:(UIView*)view;
+
+/**
+ * Constructor with parent view controller. It creates a view with text.
+ * @param viewController The parent view controller
+ * @param title The title of the window
+ * @param message The message to show to the user
+ * @return self or nil if something went wrong
+ */
 - (id)initWithViewController:(UIViewController*)viewController title:(NSString*)title message:(NSString*)message;
+
+/**
+ * Constructor with parent view and custom content
+ * @param parentView The parent view
+ * @param view The content to show
+ * @return self or nil if something went wrong
+ */
 - (id)initWithParentView:(UIView*)parentView view:(UIView*)view;
+
+/**
+ * Constructor with parent view. It creates a view with text.
+ * @param parentView The parent view
+ * @param title The title of the window
+ * @param message The message to show to the user
+ * @return self or nil if something went wrong
+ */
 - (id)initWithParentView:(UIView*)parentView title:(NSString*)title message:(NSString*)message;
+
+/**
+ * Constructor with custom content
+ * @param view The content to show
+ * @return self or nil if something went wrong
+ */
 - (id)initWithView:(UIView*)view;
+
+/**
+ * Constructor with strings. It will create a view with a message
+ * @param title The title of the window
+ * @param message The message to show to the user
+ * @return self or nil if something went wrong
+ */
 - (id)initWithTitle:(NSString*)title message:(NSString*)message;
 
+/**
+ * Shows the window
+ */
 - (void)show;
+
+/**
+ * Shows the window with a certain animation configuration
+ * @param duration The duration of the animation
+ * @param delay The delay of the animation
+ * @param options The animation options
+ * @param completion The action to perform at the end of the animation
+ */
 - (void)showWithDuration:(CGFloat)duration delay:(NSTimeInterval)delay options:(UIViewAnimationOptions)options completion:(void (^)(void))completion;
 
+/**
+ * Hides the window
+ */
 - (void)hide;
+
+/**
+ * Hides the control with a certain animation configuration
+ * @param duration The duration of the animation
+ * @param delay The delay of the animation
+ * @param options The animation options
+ * @param completion The action to perform at the end of the animation
+ */
 - (void)hideWithDuration:(CGFloat)duration delay:(NSTimeInterval)delay options:(UIViewAnimationOptions)options completion:(void (^)(void))completion;
 
+/**
+ * Hides the close button in the top corner
+ * @param hide TRUE if you want to hide the close button, or FALSE if you want to unhide it.
+ */
 -(void)hideCloseButton:(BOOL)hide;
-
 
 @end
